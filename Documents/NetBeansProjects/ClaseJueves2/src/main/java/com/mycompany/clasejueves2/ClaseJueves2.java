b3ed69e71e1eaedb48478d10d5e9542b7db3ab10
roger <rogerpalmpalac@gmail.com>
2025-09-18 21:42:34 -0600

    Carga segunda

---
+++ Documents/NetBeansProjects/ClaseJueves2/src/main/java/com/mycompany/clasejueves2/ClaseJueves2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasejueves2;
+ // Leer TXt con java, tambien obtener el cambio de divisa con webstrap 
+
+
+
+/**
+ *
+ * @author roger
+ */
+public class ClaseJueves2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
