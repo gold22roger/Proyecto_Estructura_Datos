2edbb6b985e266e9df46b38021f1c6f7d75c555c
roger <rogerpalmpalac@gmail.com>
2025-09-25 21:02:07 -0600

    Carga segunda

---
+++ Documents/NetBeansProjects/Proyecto_Estructura_Datos/src/main/java/com/mycompany/proyecto_estructura_datos/Proyecto_Estructura_Datos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_estructura_datos;
+
+/**
+ *
+ * @author roger
+ */
+public class Proyecto_Estructura_Datos {
+
+    public static void main(String[] args) {
+        int numero = 1;
+    
+    
+        String nombre = "Roger";
+        System.out.println("Hello World!" + numero + " "+ nombre);
+    }
+}
